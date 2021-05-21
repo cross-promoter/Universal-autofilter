e183f7cf022093d479d7c08f3a56b935430f9890
cross-promoter <84570430+cross-promoter@users.noreply.github.com>
2021-05-21 18:51:30 +0400

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -89,10 +89,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('🔰 NEW MOVIES 🔰', url='https://t.me/Universal_Cinemas'),
+        InlineKeyboardButton('🔰 MOVIE GROUP 🔰', url ='https://t.me/joinchat/J2jyXUN5I3hlODM1')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('OWNER', url='https://t.me/CROSS_PROMOTER')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
